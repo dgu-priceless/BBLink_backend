9fe935e15304fe21b7d47fdd7973a7b1a6578a81
shinyejin0212 <shinyejin0212@gmail.com>
2022-08-06 13:35:46 +0900

    feat : login API 및 my_settings.py 분리

---
+++ account/serializers.py
@@ -0,0 +1,23 @@
+from .models import User
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            # email = validated_data['email'],
+            # nickname = validated_data['nickname'],
+            # name = validated_data['name'],
+            # password = validated_data['password']
+            login_id = validated_data['login_id'],
+            login_password = validated_data['login_password'],
+            user_nickname = validated_data['user_nickname'],
+            current_address = validated_data['current_address'],
+            payment_method = validated_data['payment_method'],
+            user_phone = validated_data['user_phone'],
+            user_allergy = validated_data['user_allergy']
+        )
+        return user
+    class Meta:
+        model = User
+        # fields = ['nickname', 'email', 'name', 'password']
+        fields = ['login_id', 'login_password', 'user_nickname', 'current_address', 'payment_method', 'user_phone', 'user_allergy']
